55127a98d2c2024a3f8da7726377240f2a069e9a
西 <24n.misa@gmail.com>
2025-07-27 14:38:59 +0900

    README修正

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TodoApp from "@/components/todo-app"
-
-export default function Home() {
-  return (
-    <main className="container mx-auto px-4 py-8">
-      <TodoApp />
-    </main>
-  )
-}
